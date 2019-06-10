7d9efd8e929658e91ef39c5fbde33dec49f5d1df
wujianjie <181159075@qq.com>
2019-06-10 08:30:28 +0800

    day001.py

---
+++ day001.py
@@ -0,0 +1,70 @@
+#content = input("你吃饿了么？")
+#print("我们在控制台接收了："+content)
+
+# a = input("请输入a：")
+# b = input("请输入b：")
+# c = int(a) + int(b)     #只有纯数字的字符串才能进行转换
+# print(c)
+
+# content = input("请输入你是否中奖了：")
+# if content =="是":
+#     print("走上人生巅峰！！！")
+# else:
+#     print("该干啥干啥")
+#
+# content = """
+# 文能提笔安天下，
+# 武能上马定乾坤，
+# 心存谋略何人胜，
+# 古今英雄唯是君。
+# """
+# print(content)
+
+#
+# a = 66
+# b = int(input("请输入要比较的数字："))
+
+# if b == a:
+#     print("您猜对了")
+# else:
+#     if b > a :
+#         print("您输入的数字比目标数字要大")
+#     else:
+#         print("您输入的数字比目标数字要小")
+
+# if a > b:
+#     print("猜小了！")
+# elif a < b:
+#     print("猜大了！")
+# else:
+#     print("蒙对了！")
+
+
+# age = int(input("请输入您的年龄："))
+#
+# if age < 10:
+#     print("小屁孩，赶紧回家吃奶去吧！")
+# elif age >= 10 and age < 20:
+#     print("青春期叛逆小屁孩，赶紧写作业去吧！")
+# elif age >=20 and age < 30:
+#     print("开始定性，开始混社会的小屁孩！")
+# elif age >=30 and age < 40:
+#     print("也老大不小了，还不结婚等啥呢？")
+# elif age >=40 and age <50:
+#     print("家里有个不听话的小屁孩，赶紧管管吧！")
+# elif age >=50 and age <60:
+#     print("自己马上就要变成不听话的老屁孩了！")
+# elif age >=60 and age <70:
+#     print("活着还错的老屁孩！")
+# elif age >=70 and age <90:
+#     print("人生就快要结束了，抓紧时间该干啥干啥吧！")
+# else:
+#     print("该再见了这个世界！")
+
+# content = input("请输入：麻花藤")
+# if content == "麻花藤":
+#     print("真聪明，输对了！")
+# else:
+#     print("你是傻×吗？")
+
+
